a34b23fe30b4e649da6b90f6e597a3f1dad8fafb
rajvimali <rajvimali1112@gmail.com>
2024-11-24 00:45:32 +0530

    hero section complete

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.html"], // Scan all HTML files for class usage
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1d4ed8",
+        secondary: "#9333ea",
+      },
+      fontFamily: {
+        "train-one": ['"Train One"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
